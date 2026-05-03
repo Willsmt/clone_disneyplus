3932bbd1b7a925d994a2c843634d833d02f64ec5
Willsmt <willian_marthins@hotmail.com>
2026-05-03 09:10:42 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
 const sourcemaps = require("gulp-sourcemaps");
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
 function compilaSass() {
   return gulp
@@ -21,18 +22,16 @@ function comprimeImagens() {
     .pipe(gulp.dest("./dist/images"));
 }
 
-exports.default = gulp.parallel(compilaSass, comprimeImagens);
+function comprimejs() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+}
+
+exports.default = gulp.parallel(compilaSass, comprimeImagens, comprimejs);
 
 exports.watch = function () {
-  gulp.watch(
-    ".src/images/*",
-    { ignoreInitial: false },
-    gulp.series(comprimeImagens),
-  );
-
-  gulp.watch(
-    "./src/styles/*.scss",
-    { ignoreInitial: false },
-    gulp.series(compilaSass),
-  );
+  gulp.watch("./src/styles/*.scss", gulp.parallel(compilaSass));
+  gulp.watch("./src/scripts/*.js", gulp.parallel(comprimejs));
 };
